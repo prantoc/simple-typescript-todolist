971270b37ded10b7fbb65568c83803ff195f845e
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-28 23:20:14 +0600

    add list and show list

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,33 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>("#list")
 // const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
-const input = document.querySelector<HTMLInputElement>("new-task-title")
-
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = []
 form?.addEventListener('submit', e => {
   e.preventDefault();
   if (input?.value == "" || input?.value == null) return;
-  const newTask = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
-
   addListItem(newTask);
-
+  input.value = ''
 })
 
 function addListItem(task: Task) {
-
+  const item = document.createElement('li')
+  const label = document.createElement('label')
+  const checkbox = document.createElement('input')
+  checkbox.type = "checkbox"
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    console.log(task)
+  })
+  item.classList.add('list-group-item');
+  checkbox.classList.add('p-2', 'm-2');
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
 }
