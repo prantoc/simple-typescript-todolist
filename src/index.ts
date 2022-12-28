42a8a210173266223cf0c7b0347c77455e17d412
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-28 19:59:35 +0600

    set type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-import { uuid } from 'uuidv4';
+import { v4 as uuidV4 } from 'uuid';
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
 const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("new-task-form") as HTMLFormElement | null
+// const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
 const input = document.querySelector<HTMLInputElement>("new-task-title")
 
 form?.addEventListener('submit', e => {
   e.preventDefault();
   if (input?.value == "" || input?.value == null) return;
+  const newTask = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
 
-  input.value
+  addListItem(newTask);
 
 })
+
+function addListItem(task: Task) {
+
+}
